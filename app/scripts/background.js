d71fc39cff1fd995159d2e86212946e119cef2bd
inabajunmr <inabajun.for.regi@gmail.com>
2019-03-01 22:51:51 +0900

    first virsion

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,5 +1,5 @@
 `use strict`;
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.sendMessage(tab.id, "gh-big-review");
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.sendMessage(tab.id, "gh-big-review");
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,54 @@
 `use strict`;
 
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request == "gh-big-review") {
-		execute();
-	}
+chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request == "gh-big-review") {
+    execute();
+  }
 });
 
 function execute() {
-  // hide all file diff
-  Array.from( document.getElementsByClassName("file") )
-  .forEach(e => {e.classList.remove("open"); e.classList.remove("Details--on")});
+  Array.from(document.getElementsByClassName("file"))
+    .forEach(e => {
+      // Collapse all file diff
+      e.classList.remove("open");
+      e.classList.remove("Details--on");
+    });
 
-  // todo add button for done
+  Array.from(document.getElementsByClassName("file-header"))
+    .forEach(e => {
+      // when click, change background color
+      e.addEventListener("click", elem => toggle(elem));
+    });
+}
+
+function toggle(e) {
+  if (e.target.classList.contains("file-info")) {
+    // click inside
+    infoToggle(e);
+    return
+  }
+
+  if (e.target.classList.contains("file-header")) {
+    // click outside
+    headerToggle(e);
+  }
+}
+
+function headerToggle(e) {
+  if (e.target.style.backgroundColor === "gray") {
+    e.target.style.backgroundColor = "";
+    return
+  }
+
+  e.target.style.backgroundColor = "gray";
+}
+
+function infoToggle(e) {
+  if (e.target.parentNode.style.backgroundColor === "gray") {
+    e.target.parentNode.style.backgroundColor = "";
+    return
+  }
+
+  e.target.parentNode.style.backgroundColor = "gray";
+
 }
