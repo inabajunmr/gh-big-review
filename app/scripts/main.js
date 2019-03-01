b4a7bd3b0ba6615b2b1d76372d13084901bc1967
inabajunmr <inabajun.for.regi@gmail.com>
2019-03-01 13:16:53 +0900

    all diff close

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,15 @@
 `use strict`;
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	// if (request == "gh-big-review") {
-		alert(request)
-	// }
+	if (request == "gh-big-review") {
+		execute();
+	}
 });
+
+function execute() {
+  // hide all file diff
+  Array.from( document.getElementsByClassName("file") )
+  .forEach(e => {e.classList.remove("open"); e.classList.remove("Details--on")});
+
+  // todo add button for done
+}
